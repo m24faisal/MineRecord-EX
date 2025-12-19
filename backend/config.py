cb4e78b2f2c67bc32e42e8d7d697d0d4f183cbb8
m24faisal <mahirfaiz14@gmail.com>
2025-12-19 13:10:28 -0500

    fix db

--- backend/config.py
+++ backend/config.py
@@ -7,7 +7,7 @@ DB_CONFIG: Dict[str, Any] = {
     "port": "5432",
     "database": "playerData",
     "user": "postgres",
-    "password": "Faiz256!"
+    "password": "a"
 }
 
 # --- Data Collection Server Configuration ---
